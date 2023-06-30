13102e8c726863f287aaaf7c17d8964f2a9c2433
longtran2701 <longtran2701@gmail.com>
2023-06-30 10:11:59 +0700

    commit

--- test_ci_cd/lib/main.dart
+++ test_ci_cd/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:collection';
+
+import 'package:device_calendar/device_calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:timezone/timezone.dart' as tz;
 
 void main() {
   runApp(const MyApp());
@@ -7,21 +11,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page 2'),
@@ -32,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,50 +34,104 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  late DeviceCalendarPlugin _deviceCalendarPlugin;
+
+  @override
+  void initState() {
+    super.initState();
+    _deviceCalendarPlugin = DeviceCalendarPlugin();
+    _retrieveCalendars();
+  }
+
+  Future<void> _retrieveCalendars() async {
+    Result<bool> hasPermissions = await _deviceCalendarPlugin.hasPermissions();
+    if (!hasPermissions.data!) {
+      await _deviceCalendarPlugin.requestPermissions();
+    }
+    Result<List<Calendar>> calendars = await _deviceCalendarPlugin.retrieveCalendars();
+    print("length ${calendars.data?.length}");
+    for (Calendar item in calendars.data!) {
+      print(item.toJson().toString());
+      await _retrieveEvents(item.id ?? "");
+    }
+    // calendars.data?.forEach((element) async {
+
+    // });
+    // _addEvent(calendars.data?.first.id ?? "");
+  }
+
+  Future<void> _addEvent(String id) async {
+    print("_addEvent");
+
+    String title = 'Tiêu đề sự kiện';
+    String description = 'Mô tả sự kiện';
+    tz.TZDateTime startDate = tz.TZDateTime.from(DateTime.now(), tz.local);
+    tz.TZDateTime endDate = tz.TZDateTime.from(DateTime.now().add(const Duration(hours: 1)), tz.local);
+
+    Event event = Event(
+      id,
+      title: title,
+      description: description,
+      start: startDate,
+      end: endDate,
+    );
+
+    Result<String>? result = await _deviceCalendarPlugin.createOrUpdateEvent(
+      event,
+    );
+    print("result ${result?.data}");
+
+    // if (result.isSuccess) {
+    //   ScaffoldMessenger.of(context).showSnackBar(
+    //     const SnackBar(content: Text('Sự kiện đã được thêm thành công')),
+    //   );
+    // } else {
+    //   ScaffoldMessenger.of(context).showSnackBar(
+    //     SnackBar(content: Text('Lỗi khi thêm sự kiện: ${result.errorMessages}')),
+    //   );
+    // }
+  }
+
+  Future<void> _retrieveEvents(String calendarId) async {
+    print("_retrieveEvents $calendarId");
+
+    DateTime startDate = DateTime.now().subtract(const Duration(days: 7));
+    DateTime endDate = DateTime.now().add(const Duration(days: 7));
+    startDate = DateTime(2023, 8, 30);
+    endDate = DateTime(2023, 12, 30);
+
+    // print("startDate $startDate");
+    // print("endDate $endDate");
+
+    try {
+      Result<UnmodifiableListView<Event>> result = await _deviceCalendarPlugin.retrieveEvents(
+        calendarId,
+        RetrieveEventsParams(startDate: startDate, endDate: endDate),
+      );
+      print("length event ${result.data?.length}");
+
+      result.data?.forEach((element) {
+        print(element.toJson().toString());
+      });
+    } catch (e) {
+      print('Lỗi khi lấy sự kiện: $e');
+    }
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -109,7 +148,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
